ec9191d600d01a098fb766b124c518020b484313
owari <jgabrielsabatini@outlook.com>
2025-08-30 04:28:09 -0300

    continue from https://vkguide.dev/docs/new_chapter_3/building_pipeline/

---
+++ src/vk_engine.h
@@ -0,0 +1,154 @@
+﻿// vulkan_guide.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <vk_types.h>
+
+#include "vk_descriptors.h"
+
+#include "vk_pipelines.h"
+
+
+constexpr unsigned int FRAME_OVERLAP = 2;
+
+struct DeletionQueue
+{
+	std::deque<std::function<void()>> deletors;
+
+	void push_function(std::function<void()>&& function) {
+		deletors.push_back(function);
+	}
+
+	void flush() {
+		// reverse iterate the deletion queue to execute all the functions
+		for (auto it = deletors.rbegin(); it != deletors.rend(); it++) {
+			(*it)(); //call functors
+		}
+
+		deletors.clear();
+	}
+};
+
+
+struct FrameData {
+	VkSemaphore _swapchainSemaphore, _renderSemaphore;
+	VkFence _renderFence;
+	VkCommandPool _commandPool;
+	VkCommandBuffer _mainCommandBuffer;
+	DeletionQueue _deletionQueue;
+};
+
+struct ComputePushConstants {
+	glm::vec4 data1;
+	glm::vec4 data2;
+	glm::vec4 data3;
+	glm::vec4 data4;
+};
+
+struct ComputeEffect {
+	const char* name;
+
+	VkPipeline pipeline;
+	VkPipelineLayout layout;
+
+	ComputePushConstants data;
+};
+
+
+
+
+class VulkanEngine {
+public:
+
+	std::vector<ComputeEffect> backgroundEffects;
+	int currentBackgroundEffect{ 0 };
+
+
+	VkFence _immFence;
+	VkCommandBuffer _immCommandBuffer;
+	VkCommandPool _immCommandPool;
+
+
+	
+
+	DescriptorAllocator globalDescriptorAllocator;
+
+	VkDescriptorSet _drawImageDescriptors;
+	VkDescriptorSetLayout _drawImageDescriptorLayout;
+
+
+	//draw resources
+	AllocatedImage _drawImage;
+	VkExtent2D _drawExtent;
+
+	VmaAllocator _allocator;
+
+	DeletionQueue _mainDeletionQueue;
+
+	FrameData _frames[FRAME_OVERLAP];
+
+	FrameData& get_current_frame() { return _frames[_frameNumber % FRAME_OVERLAP]; };
+
+	VkQueue _graphicsQueue;
+	uint32_t _graphicsQueueFamily;
+
+	bool _isInitialized{ false };
+	int _frameNumber {0};
+	bool stop_rendering{ false };
+	VkExtent2D _windowExtent{ 1700 , 900 };
+
+	struct SDL_Window* _window{ nullptr };
+
+	static VulkanEngine& Get();
+
+	//initializes everything in the engine
+	void init();
+
+	//shuts down the engine
+	void cleanup();
+
+	//draw loop
+	void draw();
+
+	//run main loop
+	void run();
+
+	// --- omitted ---
+
+	VkInstance _instance;// Vulkan library handle
+	VkDebugUtilsMessengerEXT _debug_messenger;// Vulkan debug output handle
+	VkPhysicalDevice _chosenGPU;// GPU chosen as the default device
+	VkDevice _device; // Vulkan device for commands
+	VkSurfaceKHR _surface;// Vulkan window surface
+
+	VkSwapchainKHR _swapchain;
+	VkFormat _swapchainImageFormat;
+
+	std::vector<VkImage> _swapchainImages;
+	std::vector<VkImageView> _swapchainImageViews;
+	VkExtent2D _swapchainExtent;
+
+	VkPipeline _gradientPipeline;
+	VkPipelineLayout _gradientPipelineLayout;
+
+	void immediate_submit(std::function<void(VkCommandBuffer cmd)>&& function);
+
+private:
+	void init_imgui();
+
+	void init_pipelines();
+	void init_background_pipelines();
+	void init_descriptors();
+
+	void init_vulkan();
+	void init_swapchain();
+	void init_commands();
+	void init_sync_structures();
+	void draw_background(VkCommandBuffer cmd);
+
+	void create_swapchain(uint32_t width, uint32_t height);
+	void destroy_swapchain();
+
+	void draw_imgui(VkCommandBuffer cmd, VkImageView targetImageView);
+};
---
+++ src/vk_pipelines.h
@@ -0,0 +1,9 @@
+﻿#pragma once 
+#include <vk_types.h>
+
+namespace vkutil {
+    bool load_shader_module(const char* filePath,
+        VkDevice device,
+        VkShaderModule* outShaderModule);
+
+};
